49f37e85e65400c9b2b620d0ead3ea93e99b0194
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2024-12-10 14:25:38 +0100

    Refactoring

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,52 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
+	private final List<Integer> values;
 
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		values = new ArrayList<>(Collections.nCopies(size,0));	
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.values.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return List.copyOf(this.values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return this.values.stream()
+				.map(value -> value < values.size())
+				.toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		var afterHit = this.values.get(elem) + 1;
+		this.values.set(elem, afterHit);
+		return this.values.get(afterHit);
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return this.values.stream()
+				.map(value -> value.toString())
+				.collect(Collectors.joining("|" , "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return this.values.stream()
+				.allMatch(value -> value.equals(this.values.get(0)));
 	}
 }
